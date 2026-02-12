132a7643c6e00ccee7f90b4166c2793e0093eac0
AmaBurg <amaburges@hartford.edu>
2026-02-12 14:39:36 -0500

    Added initials
    Added my initials as strings

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,13 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
+    System.err.println("           AAA           BBBBBBBBBBBBBB                ");
+    System.err.println("          AAAAA          BBB      BBBBBB         ");
+    System.err.println("         AAA AAA         BBB       BBBBBB         ");
+    System.err.println("        AAA   AAA        BBB      BBBBBB          ");
+    System.err.println("       AAA     AAA       BBBBBBBBBBBBBB           ");
+    System.err.println("      AAAAAAAAAAAAA      BBB        BBBBB        ");
+    System.err.println("     AAA         AAA     BBB         BBBBB       ");
+    System.err.println("    AAA           AAA    BBB        BBBBBB      ");
+    System.err.println("   AAA             AAA   BBBBBBBBBBBBBBBB         ");
   }
 }
