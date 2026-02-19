1c13a332255d5e7949c98ef436ebeba6053c66df
AmaBurg <amaburges@hartford.edu>
2026-02-19 15:28:06 -0500

    Added random # generator to program 1 & began program 2

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
-//Imports the java scanner function
+import java.util.Random;
+//Imports the java scanner & random function
 
 public class Assignment1Program1 {
   public static void main(String[] args) {
     System.err.println("           AAA           BBBBBBBBBBBBBB         ");
-    System.err.println("          AAAAA          BBB      BBBBBB        ");
-    System.err.println("         AAA AAA         BBB       BBBBBB       ");
-    System.err.println("        AAA   AAA        BBB      BBBBBB        ");
+    System.err.println("          AAAAA          BBB       BBBBB        ");
+    System.err.println("         AAA AAA         BBB        BBBBB       ");
+    System.err.println("        AAA   AAA        BBB       BBBBB        ");
     System.err.println("       AAA     AAA       BBBBBBBBBBBBBB         ");
-    System.err.println("      AAAAAAAAAAAAA      BBB        BBBBB       ");
-    System.err.println("     AAA         AAA     BBB         BBBBB      ");
-    System.err.println("    AAA           AAA    BBB        BBBBBB      ");
+    System.err.println("      AAAAAAAAAAAAA      BBB         BBBB       ");
+    System.err.println("     AAA         AAA     BBB          BBBB      ");
+    System.err.println("    AAA           AAA    BBB         BBBBB      ");
     System.err.println("   AAA             AAA   BBBBBBBBBBBBBBBB       ");
 
     Scanner scan = new Scanner(System.in);
-    //Initializes scanner for user input
 
     String fiveLetterString = "";
     String threeLetterString = "";
     String reverseString = "";
-    //Initializes variables as blank strings
 
     System.err.println();
     System.err.println();
     System.err.println("Enter a 5 letter string: ");
     
     fiveLetterString = scan.nextLine(); 
-    //Sets the variable fiveLetterString to the next string that the user inputs in console
     
     threeLetterString = fiveLetterString.substring(1, (fiveLetterString.length() - 1)); 
     //Removes the first and last character of the fiveLetterString varible
@@ -35,9 +33,20 @@ public class Assignment1Program1 {
       reverseString = threeLetterString.charAt(i) + reverseString;
       //Appends the character at index(i) of threeLetterString to the front of reverseString, with i increasing by 1 for each loop
     }
-    System.err.println(reverseString);
     
+    Double fahrenheit;
+    Double celsius;
+    System.err.println("Enter a number in Fahrenheit: ");
+    fahrenheit = scan.nextDouble();
+    Double conversionFactor = (double)5/9;
+    celsius = (fahrenheit - 32) * conversionFactor;
     scan.close();
+
+    System.err.println("Generating random number...");
+    Random generator = new Random();
+    int randNum = generator.nextInt(32, 16384);
+
+    System.err.println("Your new string is: " + celsius + reverseString + randNum);
 
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    int newBase;
+    double baseMax;
+    int baseTen;
+    String conversion = "";
+    Scanner scan = new Scanner(System.in);
 
+    System.err.println("Base Number Converter: ");
+    System.err.println("Please enter a base number from 2-9: ");
+    
+    newBase = scan.nextInt();
+    baseMax = Math.pow(newBase, 4) - 1;
+    //Formula for max of a base = b^n - 1; b-base, n-# of digits
+    
+    System.err.println("The maximum, 4-digit, base 10 number in base " + newBase + " is: " + baseMax);
+    System.err.println("Enter a base 10 number in the range of 0 to " + baseMax + " to convert to base " + newBase +": ");
+    
+    baseTen = scan.nextInt();
+    
+    for (int i = baseTen; i > 0;) {
+      conversion = (i%newBase) + conversion;
+      // Appends the remainder of i, or baseTen, divided by newBase to string conversion
+      i = i/newBase;
+    }
+    
+    System.err.println(baseTen + " (base 10) = " + conversion + " (base " + newBase + ")");
+    // The beginning 0 is not being appended to variable conversion. | 2/19, 3:23pm |
+    int test = 8%4;
+    System.err.println(test);
+
+    scan.close();
   }
 }
